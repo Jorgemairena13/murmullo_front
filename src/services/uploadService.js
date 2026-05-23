098e5c899224b69d3743cea034b4a7269fb69cce
Jorge <jorgemairena13@gmail.com>
2026-05-23 18:18:38 +0200

    feat: integrar subida de imágenes en CreatePostForm y limpiar UploadButton

--- src/services/uploadService.js
+++
@@ -1,9 +0,0 @@
-import client from './client'
-
-export const uploadImage = async (file) => {
-    const formData = new FormData()
-    formData.append('photo', file)
-
-    const { data } = await client.post('/upload', formData)
-    return data
-}
