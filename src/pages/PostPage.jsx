26c440bb0db69eb4a1d13378c65bde4b27233655
Jorge <jorgemairena13@gmail.com>
2026-05-31 14:02:51 +0200

    mejorar la vista de post en movil

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -166,7 +166,7 @@ export const PostPage = () => {
         : null;
 
     return (
-        <div className="h-screen bg-black flex flex-col lg:flex-row overflow-y-auto lg:overflow-hidden">
+        <div className="min-h-screen pb-[72px] lg:pb-0 bg-black flex flex-col lg:flex-row overflow-y-auto lg:overflow-hidden">
             <div className="max-h-[50vh] lg:max-h-none lg:flex-1 flex items-center justify-center bg-black relative">
                 <button
                     onClick={() => navigate(-1)}
